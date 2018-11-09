f566b5d3ee47f21d80bcba182ff7a4813843d4d8
Rogezz <rodrigue.fotsing@student.umons.ac.be>
2018-11-09 15:35:02 +0100

    Exercice1

--- src/main/java/exercise1/Employee.java
+++ src/main/java/exercise1/Employee.java
@@ -1,15 +1,28 @@
 package exercise1;
 
-public class Employee {
-	public int computeSalary() {
-		return 0;
+public abstract class Employee {  // il faut creer la classe abstraite
+
+	private String name;
+	protected int sales = 0;
+	protected int workedHours = 0;
+
+	public Employee(String name) {
+		this.name = name;
+
 	}
 
-	public void sell() { }
+	public abstract int computeSalary();
 
-	public void workOneHour() { }
+
+	public void sell() {
+		sales ++;
+	}
+
+	public void workOneHour() {
+		workedHours ++;
+	}
 
 	public String getName() {
-		return null;
+		return name;
 	}
 }
